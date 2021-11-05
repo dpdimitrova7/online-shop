62d58404eccf310f1fc4f3388a9c501bccb5827d
dpdimitrova7 <dpdimitrova7@gmail.com>
2021-11-05 07:03:20 +0000

    Styled shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
@@ -17,7 +18,7 @@ class App extends React.Component {
       <div className='App'>
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
